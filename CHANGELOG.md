0dc624d6026ae16ff82a7adb8d7966d081e4effa
Ilton ingui <iltoningui@outlook.com>
2021-02-22 02:01:18 +0100

    Updates package version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## ## 0.0.2-alpha
+
+- Adds flutter capabilities
+- load file as resource instead of system file
+
 ## 0.0.1-alpha
 
 - Initial alpha release
--- test/ibans_dart_test.dart
+++ test/ibans_dart_test.dart
@@ -9,6 +9,7 @@ void main() {
     test('First Test', () async {
       var iban = await parse('AO06005500009209313310152');
       expect(iban.country.code, 'AO');
+      expect(iban.bank.code, '0055');
     });
   });
 }
