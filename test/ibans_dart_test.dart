52562b66c13edea2bb6a939fcfc15626873b1efd
Ilton ingui <iltoningui@outlook.com>
2021-02-22 01:53:16 +0100

    migrate lib to flutter

--- test/ibans_dart_test.dart
+++ test/ibans_dart_test.dart
@@ -1,11 +1,10 @@
 
+import 'package:flutter_test/flutter_test.dart';
 import 'package:ibans/ibans.dart';
 
 void main() {
   group('A group of tests', () {
 
-    setUp(() {
-    });
 
     test('First Test', () {
       var iban = parse('AO06005500009209313310152');
